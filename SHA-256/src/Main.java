08a71282e4167aa922cc2a466f24a89026775fe2
Isaac <isaacgames95@gmail.com>
2023-08-24 00:49:47 -0300

    update main

--- SHA-256/src/Main.java
+++ SHA-256/src/Main.java
@@ -8,7 +8,84 @@ public class Main
 		for(int i = 0; i < remainingZeros; i++){
 			binSB.append("0");
 		}
-		return binSB.toString() + bin;
+		return binSB + bin;
+	}
+
+	public static String xorSum(String s1, String s2){
+		StringBuilder result = new StringBuilder();
+		if(s1.length() != s2.length()){
+			System.out.println("Different sizes of strings");
+			return null;
+		}else{
+			for(int i = 0; i < s1.length(); i++){
+				if(s1.charAt(i) == s2.charAt(i)){
+					result.append("0");
+				}else {
+					result.append("1");
+				}
+			}
+		}
+		return result.toString();
+	}
+
+	public static String xorSum(String s1, String s2, String s3){
+		String firstStep = xorSum(s1, s2);
+		return xorSum(firstStep, s3);
+	}
+
+	public static String rightRotate (String toRotate, int piece){
+		String strPiece = toRotate.substring(toRotate.length()-piece);
+		String strRemaining = toRotate.substring(0, toRotate.length()-piece);
+		return strPiece + strRemaining;
+	}
+
+	public static String rightShift(String toShift, int steps){
+		String rightPiece = toShift.substring(0, toShift.length() - steps);
+		return toBitSize(rightPiece, 32);
+	}
+
+	public static String addBinary(String x, String y){
+		int i = x.length() - 1, j = y.length() - 1;
+		int carry = 0;
+		StringBuilder result = new StringBuilder();
+		while (i >= 0 || j >= 0) {
+			int sum = carry;
+			if (i >= 0) {
+				sum += x.charAt(i) - '0';
+			}
+			if (j >= 0) {
+				sum += y.charAt(j) - '0';
+			}
+			if (sum == 0 || sum == 1) {
+				result.append(sum);
+				carry = 0;
+			}
+			else if (sum == 2) {
+				result.append("0");
+				carry = 1;
+			}
+			else {
+				result.append("1");
+				carry = 1;
+			}
+			i--;
+			j--;
+		}
+		if (carry == 1) {
+			result.append("1");
+		}
+		String finalResult = result.reverse().toString();
+		return finalResult.substring(finalResult.length()-32);
+	}
+
+	public static String addBinary(String x, String y, String z) {
+		String firstStep = addBinary(x, y);
+		return addBinary(firstStep, z);
+	}
+
+	public static String addBinary(String x, String y, String z, String a) {
+		String firstStep = addBinary(x, y, z);
+		return addBinary(firstStep, a);
 	}
 
     public static void main(String[] args) {
@@ -69,6 +146,13 @@ public class Main
 			lastIndex += 32;
 			System.out.println("w[" + i + "] " +w[i]);
 		}
-        
+
+		for (int i = 16; i < w.length ; i++) {
+			String mixStr0 = xorSum(rightRotate(w[i-15], 7), rightRotate(w[i-15], 18), rightShift(w[i-15], 3));
+			String mixStr1 = xorSum(rightRotate(w[i-2], 17), rightRotate(w[i-2], 19), rightShift(w[i-2], 10));
+			w[i] = addBinary(w[i-16], mixStr0, w[i-7], mixStr1);
+			System.out.println("w[" + i + "] " +w[i]);
+		}
+
     }
 }
